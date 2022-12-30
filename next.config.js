4281c41da4a9df4f2d98ddfda0ed2d9c352f3096
Juan Herrera <david.juanherrera@gmail.com>
2022-12-30 15:05:02 +0100

    fix config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-/** @type {import('next').NextConfig} */
-
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 const { withSentryConfig } = require('@sentry/nextjs');
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const moduleExports = {
   // Your existing module.exports
-  // reactStrictMode: true,
+  reactStrictMode: true,
   sentry: {
     // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
     // for client-side builds. (This will be the default starting in
@@ -36,5 +37,3 @@ const sentryWebpackPluginOptions = {
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
-
-module.exports = nextConfig;
