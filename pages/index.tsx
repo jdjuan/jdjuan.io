334ed8ca42d2af08de82d0babbcf76d9d336f02d
Juan Herrera <david.juanherrera@gmail.com>
2023-01-09 07:23:14 +0100

    add grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,15 @@ export default function Home() {
       <Head>
         <title>Juan Herrera</title>
       </Head>
-      <main className={`${inter.className} mx-auto max-w-6xl text-slate-300`}>
+      <main
+        className={`${inter.className} max-w-4xl bg-slate-900 p-10 text-slate-300 sm:m-10 md:m-10 md:p-10 md:pr-6 lg:mx-auto lg:p-20 lg:pr-10`}
+      >
         {/* FIRST SCREEN */}
-        <div className='grid gap-10 p-10 sm:grid-cols-2 md:gap-20 md:gap-y-8 md:p-20'>
+        {/* max-sm:landscape:gap-8 */}
+        <div className='grid items-center gap-10 sm:grid-cols-2 sm:gap-14 md:gap-y-8 lg:gap-2 portrait:gap-16 landscape:gap-14'>
           {/* ABOUT */}
-          <h1 className='text-3xl sm:col-span-2'>Juan Herrera</h1>
           <div className=''>
+            <h1 className='mb-8 text-3xl'>Juan Herrera</h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempor nisi diam. Interdum et. Lorem
               ipsum dolor sit amet
@@ -25,22 +28,18 @@ export default function Home() {
             </p>
           </div>
           {/* SQUARES */}
-          <div className='grid grid-cols-2 gap-8 sm:gap-10 max-md:landscape:gap-5'>
+          <div className='grid grid-cols-2 gap-8 sm:gap-5 md:gap-7 lg:gap-8 xl:gap-12 max-md:landscape:gap-5'>
             {["Behavior", "Communication", "Relationships", "Software Development"].map((value, index) => (
-              // <div key={index} className='aspect-square min-w-[6rem] max-w-[20rem] bg-slate-500'>
               <div
-                key={index}
-                className={cx("flex aspect-square w-full max-w-[10rem] items-center justify-center bg-slate-500", {
-                  "justify-self-end": index % 2 === 0,
-                })}
+                key={value}
+                className={cx(
+                  "flex aspect-square w-full max-w-[8rem] items-center justify-center bg-slate-700 text-center",
+                  { "justify-self-end": index % 2 === 0 }
+                )}
               >
                 {value}
               </div>
             ))}
-            {/* <div className='bg-slate-500'></div>
-            <div className='bg-slate-500'></div>
-            <div className='bg-slate-500'></div>
-            <div className='bg-slate-500'></div> */}
           </div>
         </div>
       </main>
