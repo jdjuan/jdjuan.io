9b87c42060444675cb75221ed1d3e4565d342dc3
Juan Herrera <david.juanherrera@gmail.com>
2023-04-08 10:35:30 +0200

    add tandem gpt

--- content/highlights.content.tsx
+++ content/highlights.content.tsx
@@ -62,6 +62,24 @@ export const highlights: Highlight[] = [
     description: "Projects I'm proud of",
     items: [
       {
+        title: "Tandem GPT",
+        link: "https://www.tandem-gpt.com/",
+        isExternalLink: true,
+        topics: ["Practice languages with AI", "Launched in 2023", "People said it was cool"],
+        icon: (
+          <svg xmlns='http://www.w3.org/2000/svg' height='100%' width='100%' viewBox='0 0 24 24'>
+            <path
+              className='fill-teal-400 dark:fill-teal-300'
+              d='M22 17a1 1 0 0 1-2 0v-5a8 8 0 1 0-16 0v5a1 1 0 0 1-2 0v-5a10 10 0 1 1 20 0v5z'
+            />
+            <path
+              className='fill-teal-800 dark:fill-teal-700'
+              d='M7 12a2 2 0 0 1 2 2v6a2 2 0 1 1-4 0v-6c0-1.1.9-2 2-2zm10 0a2 2 0 0 1 2 2v6a2 2 0 1 1-4 0v-6c0-1.1.9-2 2-2z'
+            />
+          </svg>
+        ),
+      },
+      {
         title: "HeadScroll",
         link: "https://headscroll.io/",
         isExternalLink: true,
@@ -97,24 +115,24 @@ export const highlights: Highlight[] = [
           </svg>
         ),
       },
-      {
-        title: "Der Die Das",
-        link: "http://derdiedas.jdjuan.io",
-        isExternalLink: true,
-        topics: ["German genders", "Built in 2020", "278 users"],
-        icon: (
-          <svg xmlns='http://www.w3.org/2000/svg' height='100%' width='100%' viewBox='0 0 24 24'>
-            <path
-              className='fill-teal-400 dark:fill-teal-300'
-              d='M10.41 10l1.3 1.3a1 1 0 0 1-1.42 1.4L9 11.42l-3.3 3.3a1 1 0 1 1-1.4-1.42L7.58 10l-1.3-1.3a1 1 0 0 1 1.42-1.4L9 8.58l.54-.54A5 5 0 0 0 10.98 5H3a1 1 0 1 1 0-2h5V2a1 1 0 1 1 2 0v1h5a1 1 0 0 1 0 2h-2.02a7 7 0 0 1-2.03 4.46l-.54.54z'
-            />
-            <path
-              className='fill-teal-800 dark:fill-teal-700'
-              d='M13.33 18l-1.4 3.38a1 1 0 0 1-1.85-.76l5-12a1 1 0 0 1 1.84 0l5 12a1 1 0 0 1-1.84.76L18.67 18h-5.34zm.84-2h3.66L16 11.6 14.17 16z'
-            />
-          </svg>
-        ),
-      },
+      // {
+      //   title: "Der Die Das",
+      //   link: "http://derdiedas.jdjuan.io",
+      //   isExternalLink: true,
+      //   topics: ["German genders", "Built in 2020", "278 users"],
+      //   icon: (
+      //     <svg xmlns='http://www.w3.org/2000/svg' height='100%' width='100%' viewBox='0 0 24 24'>
+      //       <path
+      //         className='fill-teal-400 dark:fill-teal-300'
+      //         d='M10.41 10l1.3 1.3a1 1 0 0 1-1.42 1.4L9 11.42l-3.3 3.3a1 1 0 1 1-1.4-1.42L7.58 10l-1.3-1.3a1 1 0 0 1 1.42-1.4L9 8.58l.54-.54A5 5 0 0 0 10.98 5H3a1 1 0 1 1 0-2h5V2a1 1 0 1 1 2 0v1h5a1 1 0 0 1 0 2h-2.02a7 7 0 0 1-2.03 4.46l-.54.54z'
+      //       />
+      //       <path
+      //         className='fill-teal-800 dark:fill-teal-700'
+      //         d='M13.33 18l-1.4 3.38a1 1 0 0 1-1.85-.76l5-12a1 1 0 0 1 1.84 0l5 12a1 1 0 0 1-1.84.76L18.67 18h-5.34zm.84-2h3.66L16 11.6 14.17 16z'
+      //       />
+      //     </svg>
+      //   ),
+      // },
     ],
   },
   {
