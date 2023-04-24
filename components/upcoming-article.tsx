8112ba774bb5b61d0ef91921f5ce6a61a5d0b164
Juan Herrera <david.juanherrera@gmail.com>
2023-04-24 18:05:57 +0200

    add new subscription

--- components/upcoming-article.tsx
+++ components/upcoming-article.tsx
@@ -1,11 +1,17 @@
 const UpcomingArticle = () => {
   return (
     <p className='mt-3 rounded-md bg-neutral-200 p-2 text-xs font-light text-neutral-600 shadow-md dark:bg-slate-800 dark:font-normal dark:text-slate-500 sm:text-sm'>
-      There&apos;s an upcoming article on this topic.{" "}
-      <a className='text-neutral-900 underline dark:text-slate-400' href='mailto:david.juanherrera@gmail.com'>
-        Introduce yourself
+      There&apos;s an upcoming article on this topic. <br />
+      Subscribe{" "}
+      <a
+        className='text-neutral-900 underline dark:text-slate-400'
+        target={"_blank"}
+        href='http://eepurl.com/hp0sUn'
+        rel='noreferrer'
+      >
+        here
       </a>{" "}
-      in an email get an update, or simply come back in a few weeks.
+      to get an update, or come back later.
     </p>
   );
 };
--- components/updates.tsx
+++ components/updates.tsx
@@ -3,15 +3,15 @@ const Updates = () => {
     <div className='mb-16 text-sm'>
       <h2 className='mb-4 font-headline text-4xl'>Updates</h2>
       <p className='text-base font-light dark:font-extralight dark:text-slate-300'>
-        Once a month I share my latest insights over email. To receive them,{" "}
-        <a className='underline dark:text-slate-200' href='mailto:david.juanherrera@gmail.com'>
-          introduce yourself
+        Once a month I share my latest insights over email. <br /> To receive them, subscribe{" "}
+        <a className='underline dark:text-slate-200' target={"_blank"} href='http://eepurl.com/hp0sUn' rel='noreferrer'>
+          here
         </a>
         .
       </p>
-      <p className='mt-2 font-light italic text-neutral-400 dark:font-extralight dark:text-slate-300'>
+      {/* <p className='mt-2 font-light italic text-neutral-400 dark:font-extralight dark:text-slate-300'>
         I reply to all emails.
-      </p>
+      </p> */}
     </div>
   );
 };
