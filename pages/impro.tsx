5b07cf7675a76cb6a3d1aca730d9a4402faf2280
Juan Herrera <juan.herrera@viable.at>
2024-10-12 08:50:49 -0500

    add impro url

---
+++ pages/impro.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const Improv = () => {
+  useEffect(() => {
+    window.location.href = "https://www.meetup.com/manizales-impro-meetup/";
+  }, []);
+
+  return null;
+};
+
+export default Improv;
